276bc446286d1ca513bdc4fb2c0ff443402ff5dd
Gio <gio@gtc.co>
2025-12-03 13:42:39 -0800

    Starting project with minor changes

---
+++ database/migrations/2025_12_03_005828_create_enrollments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id('enrollment_id');
+            $table->integer('tenant_id');
+            $table->integer('student_id');
+            $table->integer('course_id');
+            $table->date('enrollment_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
