d374dd8b6b74fe4a4df6929a42001de8e3567d66
Gio <gio@gtc.co>
2025-12-19 15:22:30 -0800

    Adding students controller functions and web paths

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class StudentController extends Controller
 {
-    //
+    public function index() {
+        $schoolId = Auth::user()->school_id;
+        $students = Student::where('school_id', $schoolId)->get();
+        return Inertia::render('student/index', [
+            'school_id' => $schoolId,
+            'students' => $students
+        ]);
+    }
+
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'first_name' => 'required|string|max:50',
+            'last_name' => 'required|string|max:50',
+            'grade' => 'required|integer',
+        ]);
+
+        $validated["tenant_id"] = Auth::user()->school_id;
+        Student::create($validated);
+        return Redirect::route('student.index');
+    }
+
+    public function update(Request $request, $id) {
+        $validated = $request->validate([
+            'first_name' => 'required|string|max:50',
+            'last_name' => 'required|string|max:50',
+            'grade' => 'required|integer',
+        ]);
+        $student = Student::where('school_id', Auth::user()->school_id)->findOrFail($id);
+        $student->update($validated);
+        return Redirect::route('student.index');
+    }
+    
+    public function destroy($id) {
+        $student = Student::where('tenant_id', Auth::user()->school_id)->findOrFail($id);
+        $student->delete();
+        return Redirect::route('student.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 
 Route::get('/', function () {
@@ -20,6 +21,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/teachers', [TeacherController::class, 'store'])->name('teacher.store');
     Route::put('/teachers/{id}', [TeacherController::class, 'update'])->name('teacher.update');
     Route::delete('/teachers/{id}', [TeacherController::class, 'destroy'])->name('teacher.destroy');
+
+    Route::get('/students', [StudentController::class, 'index'])->name('student.index');
+    Route::post('/students', [StudentController::class, 'store'])->name('student.store');
+    Route::put('/students/{id}', [StudentController::class, 'update'])->name('student.update');
+    Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('student.destroy');
 });
 
 require __DIR__.'/settings.php';
